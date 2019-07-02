7c3c343c0020c6677a409ca121544719768ad89b
Vladimir Chervanev <vlad.chervanev@rigup.com>
2019-07-01 19:13:56 -0500

    Updates

---
+++ precache-manifest.9611fb342082641b5b33ddd3b5c63dd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad70a9e3844e65ba264b9bf492d57e0b",
+    "url": "/mazzze/index.html"
+  },
+  {
+    "revision": "301ad849a7c0dd4ed692",
+    "url": "/mazzze/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mazzze/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "301ad849a7c0dd4ed692",
+    "url": "/mazzze/static/js/main.b9e2d134.chunk.js"
+  },
+  {
+    "revision": "7144c0a1b7172a9f990a",
+    "url": "/mazzze/static/js/runtime~main.1f4b1629.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mazzze/static/media/logo.5d5d9eef.svg"
+  }
+]);
